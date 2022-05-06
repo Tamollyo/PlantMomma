3012c080698427fc92267a311076ba10e12ca010
Molly Obermeier <obermeier.mol@gmail.com>
2022-05-06 15:48:11 -0600

    form values

--- plant-momma/src/pages/PlantDetails.vue
+++ plant-momma/src/pages/PlantDetails.vue
@@ -3,20 +3,26 @@
     <PlantCard :key="plant.id" v-for="plant in plants" :image="plant.image" :name="plant.name" :description="plant.description"/>
     <div>
       <div>
-         <h3>Add Plant</h3>
+         <h3>Add a Plant</h3>
       </div>
         <div class="field">
           <div>
             <textarea class="textarea" placeholder="Plant Name" v-model="form.name"></textarea>
           </div>
           <div>
-            <textarea class="textarea" placeholder="Plant Name" v-model="form.name"></textarea>
+            <textarea class="textarea" placeholder="Description" v-model="form.description"></textarea>
           </div>
           <div>
-            <textarea class="textarea" placeholder="Plant Name" v-model="form.name"></textarea>
+            <textarea class="textarea" placeholder="Sun" v-model="form.sun"></textarea>
           </div>
           <div>
-            <textarea class="textarea" placeholder="Plant Name" v-model="form.name"></textarea>
+            <textarea class="textarea" placeholder="Schedule" v-model="form.schedule"></textarea>
+          </div>
+          <div>
+            <textarea class="textarea" placeholder="Maintenance" v-model="form.maintenance"></textarea>
+          </div>
+          <div>
+            <textarea class="textarea" placeholder="Image" v-model="form.image"></textarea>
           </div>
         </div>
     </div>
