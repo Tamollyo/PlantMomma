21afbd2c9100cc2c6cb5722664354fa79df1c6e2
Molly Obermeier <obermeier.mol@gmail.com>
2022-05-10 10:02:56 -0600

    about

--- plant-momma/src/pages/AboutPage.vue
+++ plant-momma/src/pages/AboutPage.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>About</div>
+  <div>
+    <div>About</div>
+    <div>
+      <h2>Welcome to the plant app for plant lovers and plant killers. </h2>
+      <a href="https://github.com/Tamollyo/PlantMomma">GitHub FrontEnd Repository</a>
+      <a href="">GitHub BackEnd Repository</a>
+    </div>
+    <div>
+      <h2>Author's Contact</h2>
+      <a href="https://www.linkedin.com/in/marie-obermeier-6b6b03158/">LinkedIn</a>
+    </div>
+  </div>
 </template>
 
 <script>
