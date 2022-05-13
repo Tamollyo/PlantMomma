19c7dc5c539e8a337829bd846858172f45f222d9
Molly Obermeier <obermeier.mol@gmail.com>
2022-05-13 06:28:25 -0600

    delete tasks

--- plant-momma/src/components/TaskCard.vue
+++ plant-momma/src/components/TaskCard.vue
@@ -9,16 +9,30 @@
     <div>
       {{ schedule }}
     </div>
+    <button class="delete" @click="onDelete">
+        Delete
+    </button>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
+  const BASE_URL = 'http://localhost:3001/api'
+  
   export default {
     name: 'TaskCard',
     props: {
       tag: String,
       description: String,
       schedule: String,
-    },
+    }, 
+    methods: {
+      async onDelete(e) {
+        e.preventDefault()
+        const res = await axios.delete(`${BASE_URL}/plant/${this.id}`)
+        console.log(res)
+        this.$emit('deletePlant')
+      }
+    }
   }
 </script>
